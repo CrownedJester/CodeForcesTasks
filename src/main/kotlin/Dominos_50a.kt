88bd9bd583c79e882639e0b1402407a72699c978
CrownedJester <kiyoshisatoshy@gmail.com>
2022-12-22 19:23:10 +0300

    Solve 50a

--- src/main/kotlin/Dominos_50a.kt
+++ src/main/kotlin/Dominos_50a.kt
@@ -1,9 +1,35 @@
 fun main() {
-    val (m, n) = readln()
+    val inputData = readln()
         .split(" ")
         .map {
             it.toInt()
         }
 
-    print(m * n / 2)
+    val dominoField = initDominoField(inputData[0], inputData[1])
+    print(countPossibilities(dominoField))
+}
+
+private fun initDominoField(row: Int, col: Int): Array<Array<Int>> =
+    Array(row) { Array(col) { 0 } }
+
+private fun countPossibilities(matrix: Array<Array<Int>>): Int {
+    var count = 0
+    for (i in matrix.indices) {
+        for (j in matrix[0].indices) {
+            if (j != matrix[0].indices.last && matrix[i][j] == 0 && matrix[i][j + 1] == 0) {
+                matrix[i][j] = 1
+                matrix[i][j + 1] = 1
+                count++
+
+            } else if (i != matrix.indices.last && matrix[i][j] == 0 && matrix[i + 1][j] == 0) {
+                matrix[i][j] = 1
+                matrix[i + 1][j] = 1
+                count++
+            } else {
+                continue
+            }
+        }
+    }
+
+    return count
 }
